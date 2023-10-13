728a030231334c147a77d263fa10b0e49584fbe6
quangminhgiap <minh.giap@octa.vn>
2023-10-13 11:11:17 +0700

    update api login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,18 +2,18 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '../models/user.model';
 
-@Controller('user')
+@Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   // [GET] /user
-  @Get()
+  @Get('/users')
   async findAllUsers(): Promise<User[] | undefined> {
     return this.userService.findAllUsers();
   }
 
   // [GET] /user/:id
-  @Get(':id')
+  @Get('/user/:id')
   async findUserById(@Param('id') id: string): Promise<User | undefined> {
     return this.userService.findUserById(id);
   }
